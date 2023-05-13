4c07c91e6e369ae6d7308bd4618380d6973e1ccb
073_Habel <110342947+Habel2@users.noreply.github.com>
2023-05-13 19:53:45 +0700

    Update Main.java

--- No 4/src/Main.java
+++ No 4/src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         //Fisika menghitung ketinggian maksimum pada gerak vertikal ke atas
-        fisika soal1 = new fisika(20);
-        fisika soal2 = new fisika();
-        System.out.println("Ketinggian maksimum adalah adalah " + soal1.rumus());
-        System.out.println("Ketinggian maksimum adalah  "+ soal2.rumus(30, 10));
+        Fisika soal1 = new Fisika(20);
+        Fisika soal2 = new Fisika();
+        System.out.println("Ketinggian maksimum adalah adalah " + soal1.Rumus());
+        System.out.println("Ketinggian maksimum adalah  "+ soal2.Rumus(30, 10));
 
         //Matematika menghitung luas permukaan bola
-        matematika matematika1 = new matematika(7.5);
-        matematika matematika2 = new matematika();
-        System.out.println("Luas permukaan bola adalah " + matematika1.rumus());
-        System.out.println("Luas permukaan bola adalah " + matematika2.rumus(10));
+        Matematika matematika1 = new Matematika(7.5);
+        Matematika matematika2 = new Matematika();
+        System.out.println("Luas permukaan bola adalah " + matematika1.Rumus());
+        System.out.println("Luas permukaan bola adalah " + matematika2.Rumus(10));
 
     }
 
